c6832e4b3647793882f27666d023bbbbebc57e7b
Ndjock Michel <ndjockjunior@gmail.com>
2021-08-07 23:38:52 +0100

    adjusted api base url passed to https protocol

--- nuxt.config.js
+++ nuxt.config.js
@@ -87,7 +87,7 @@ export default {
   },
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {
-    baseURL: 'http://api.safira3d.com', // Used as fallback if no runtime config is provided
+    baseURL: 'https://api.safira3d.com', // Used as fallback if no runtime config is provided
     // baseURL: 'http://localhost:3001', // Used as fallback if no runtime config is provided
     retry: { retries: 3 },
   },
